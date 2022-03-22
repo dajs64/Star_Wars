8233997064970509ec04a6f7808064646b338e65
dajs64 <100155199+dajs64@users.noreply.github.com>
2022-03-22 08:37:04 -0400

    Update act2.sh

--- act2.sh
+++ act2.sh
@@ -11,3 +11,12 @@
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
 
 # 7. Move the millenium_falcon into the death_star.
+
+
+#1 cd rebellion && touch princess_leia.txt && echo "helpme, Obi-Wan...You're my only hope." >> princess_leia.txt
+#2 touch obi_wan.txt
+#3 touch luke_skywalker.txt
+#4 mkdir millenium_falcon
+#5 touch han_solo.txt chewbacca.txt
+#6 cd .. && mv luke_skywalker.txt obi_wan.txt princess_leia.txt millenium_falcon
+#7 cd ../.. && mv rebellion/millenium_falcon empire/death_star
